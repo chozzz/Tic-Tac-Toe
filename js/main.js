e8631df31c6e2354a9fd7ae64a4da04cc1ec2810
chozzz <vadz77@hotmail.com>
2016-01-22 17:24:17 +0800

    Added functional winning check feature

--- js/main.js
+++ js/main.js
@@ -1,26 +1,155 @@
-var totalRows = 3;
-
-
-function createBoardTable()
-{
-    'use strict';
-    $('#boardTbl').empty();
-    for (let r = 0; r < totalRows; r++)
+Array.prototype.allValuesSame = function() {
+    for(var i = 1; i < this.length; i++)
     {
-        var tblRow = $("<tr>").attr('id', 'row_'+r);
-        for (let c = 0; c < totalRows; c++)
-        {
-            var tblCol = $("<td>").data('row', r).data('col', c);
-            tblRow.append(tblCol);
-        }
-        $('#boardTbl').append(tblRow);
+        if(this[i] !== this[0])
+            return false;
     }
+    return true;
 }
 
-function initGame()
-{
-    createBoardTable();
-}
+var AI = {
+    
+};
+
+var Game = {
+    currentMove: 1,
+    totalRows: 3,
+    isRunning: false,
+    
+    createBoard: function() {
+        'use strict';
+        // Empty the current board.
+        $('#boardTbl').empty();
+        
+        // Creating the table cells.
+        for (let r = 1; r <= this.totalRows; r++)
+        {
+            var tblRow = $("<tr>").attr('id', 'row_'+r);
+            for (let c = 1; c <= this.totalRows; c++)
+            {
+                var tblCol = $("<td class='selected'>").data('row', r).data('col', c);
+                tblRow.append(tblCol);
+            }
+            $('#boardTbl').append(tblRow);
+        }
+    },
+    
+    start: function() {
+        Game.isRunning = true;
+        $('#boardTbl td').removeClass('selected')
+        // Attaching the event handlers to every single table cells.
+        .one('click', function(){
+            var text = Game.currentMove++ % 2 ? 'X' : 'O';
+            $(this).addClass('selected').text(text);
+            var res = Game.checkWinner();
+            if (res !== false)
+            {
+                Game.stop();
+            }
+        });
+    },
+    
+    stop: function() {
+        Game.isRunning = false;
+        $('#boardTbl td').addClass('selected').off('click');
+    },
+    
+    checkWinner: function() {
+        'use strict';
+        // Check all horizontal lines
+        let hRes = Game.getHorizontalResult();
+        for (let r = 0; r < this.totalRows; r++)
+        {
+            let val = hRes[r][0];
+            if (val != "" && hRes[r].allValuesSame())
+            {
+                console.log("'" + val + "' is the winner horizontally.");
+                return val;
+            }
+        }
+        
+        // Check all vertical lines
+        let vRes = Game.getVerticalResult();
+        for (let c = 0; c < this.totalRows; c++)
+        {
+            let val = vRes[c][0];
+            if (val != "" && vRes[c].allValuesSame())
+            {
+                console.log("'" + val + "' is the winner vertically.");
+                return val;
+            }
+        }
+        
+        // Check all diagonal lines.
+        let dRes = Game.getDiagonalResult();
+        for (let i = 0; i < dRes.length; i++)
+        {
+            let val = dRes[i][0];
+            if (val != "" && dRes[i].allValuesSame())
+            {
+                console.log("'" + val + "' is the winner diagonally.");
+                return val;
+            }
+        }
+        
+        return false;
+    },
+    
+    getDiagonalResult: function() {
+        'use strict';
+        let diagonalVals = [];
+        
+        let dArr1 = [];
+        for (let i = 0; i < this.totalRows; i++)
+        {
+            let text = $('#boardTbl tr').eq(i).children('td').eq(i).text();
+            dArr1.push(text);
+        }
+        
+        let dArr2 = [];
+        for (let i = 0, x = this.totalRows - 1; x >= 0; x--)
+        {
+            let text = $('#boardTbl tr').eq(i++).children('td').eq(x).text();
+            dArr2.push(text);
+        }
+        
+        diagonalVals.push(dArr1);
+        diagonalVals.push(dArr2);
+        return diagonalVals;
+    },
+    
+    getHorizontalResult: function() {
+        'use strict';
+        let horizontalVals = [];
+        for (let r = 0; r < this.totalRows; r++)
+        {
+            let rowArr = [];
+            for (let c = 0; c < this.totalRows; c++)
+            {
+                let text = $('#boardTbl tr').eq(r).children('td').eq(c).text();
+                rowArr.push(text);
+            }
+            horizontalVals.push(rowArr);
+        }
+        return horizontalVals;
+    },
+    
+    getVerticalResult: function() {
+        'use strict';
+        let verticalVals = [];
+        for (let r = 0; r < this.totalRows; r++)
+        {
+            let colArr = [];
+            for (let c = 0; c < this.totalRows; c++)
+            {
+                let text = $('#boardTbl tr').eq(c).children('td').eq(r).text();
+                colArr.push(text);
+            }
+            verticalVals.push(colArr);
+        }
+        return verticalVals;
+    },
+};
 
 function initPreGame()
 {
@@ -28,20 +157,22 @@ function initPreGame()
     if ((isNaN(requestedRows)) || (requestedRows < 3 || requestedRows > 9))
     {
         alert("Requested row is set to '3' by default.");
-        totalRows = 3;
+        Game.totalRows = 3;
     }
     else
     {
-        totalRows = requestedRows;
+        Game.totalRows = requestedRows;
     }
     $('#game').show();
-    initGame();
+    Game.createBoard();
+    Game.start();
 }
 
 $(function(){
     
-    initPreGame();
     /*
+    initPreGame();
+    */
     $('#preModal').on('hidden.bs.modal', function () {
         initPreGame();
     }).modal({
@@ -54,5 +185,4 @@ $(function(){
         $("#preModal").modal('hide');
         ev.preventDefault();
     });
-    */
 });
